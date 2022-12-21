7e4b09ce27f14bea97af094d4f051cbeb7c7c76a
Mauro Benelli Ost <mauro.ost@hotmail.com>
2022-12-21 15:53:06 -0300

    Added: Animated form display

--- modulo-jquery-1/main.js
+++ modulo-jquery-1/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    console.log($('header button'))
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+    })
 })
