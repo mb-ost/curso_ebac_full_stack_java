21235d637dd60e75571d6521d149c351d58e6ce2
Mauro Benelli Ost <mauro.ost@hotmail.com>
2022-12-21 20:47:13 -0300

    Finalized jquery image galery built in class

--- modulo-jquery-1/main.js
+++ modulo-jquery-1/main.js
@@ -9,5 +9,19 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e) {
         e.preventDefault();
+
+        const urlImg = $('#endereco-img-nova').val()
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${urlImg}">`) .appendTo(novoItem);
+        $(` 
+            <div class="overlay-imagem-link">
+                <a href="{urlImg}" target="_blank" title="Ver imagem em tamanho real"
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `) .appendTo(novoItem);
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000)
+        $('#endereco-img-nova').val('')
     })
 })
