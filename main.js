b6a7ad6bfb81815c6f0c86ac9174b0dca7aa5286
Mauro Benelli Ost <mauro.ost@hotmail.com>
2022-12-23 16:24:16 -0300

    Added: Show/Hide description to tasks

--- main.js
+++ main.js
@@ -4,13 +4,49 @@ TODO:
         validate fields title and description
         add new title, description and done=false to task list array
         render table with new information
-    when + button pressed:
-        options and plus button must fill 2 rows: rowspan="2"
-        description display block
-    when - button pressed:
-        option and plus button must fill 1 row
-        description display none
-    when task title or description is pressed:
-        done=true
-        .done -> text-decration: line-through
 */
+$('tr').children('.descricao').hide();
+$(document).ready(function () {
+    //---------Registrar Valores no formulario
+    $('form').on('submit', function(e){
+        e.preventDefault();
+    });
+
+    //---------Detectar click para expandir ou retrair Descrição---------
+    $('[data-toggle="mostrar-descricao"]').click(function () {
+        if($(this).attr('rowspan') == 1){
+            expandirDescricao($(this));
+        } else {
+            retrairDescricao($(this));
+        }
+        
+    })
+    //--------Riscar valores da tarefa-------------------------------------
+    $('[data-toggle="clica-titulo"]').click(function () {
+        $(this).addClass('terminada');
+        $(this).parent().next().children('.descricao').addClass('terminada');
+    })
+    $('[data-toggle="clica-tarefa"]').click(function () {
+        $(this).addClass('terminada');
+        $(this).parent().prev().children('.nome-tarefa').addClass('terminada');
+    })
+
+});
+//------------Func Expandir Desc Tabela----------------------------------
+function expandirDescricao(item) {
+    item.parent().next().children('.descricao').slideDown(50);
+    item.parent().next().children('.descricao').attr('colspan', 1);
+    item.attr('rowspan', 2);
+    item.parent().children('.delete-tarefa').attr('rowspan', 2);
+    item.children('i').removeClass('fa-plus');
+    item.children('i').addClass('fa-minus');
+}
+//------------Func Retrair Desc Tabela----------------------------------
+function retrairDescricao(item) {
+    item.attr('rowspan', 1);
+    item.parent().children('.delete-tarefa').attr('rowspan', 1);
+    item.parent().next().children('.descricao').attr('colspan', 3);
+    item.parent().next().children('.descricao').slideUp(50);
+    item.children('i').removeClass('fa-minus');
+    item.children('i').addClass('fa-plus');
+}
