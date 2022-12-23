a05da5bce30ff51d86e20596fccd4d78d28733ee
Mauro Benelli Ost <mauro.ost@hotmail.com>
2022-12-23 11:37:17 -0300

    Added: Hide button and js TODO list

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+TODO:
+    when adicionar + is pressed:
+        validate fields title and description
+        add new title, description and done=false to task list array
+        render table with new information
+    when + button pressed:
+        options and plus button must fill 2 rows: rowspan="2"
+        description display block
+    when - button pressed:
+        option and plus button must fill 1 row
+        description display none
+    when task title or description is pressed:
+        done=true
+        .done -> text-decration: line-through
+*/
