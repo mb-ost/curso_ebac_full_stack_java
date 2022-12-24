79b5bbeb0e430aa0670c8e4ce681889236f29d15
Mauro Benelli Ost <mauro.ost@hotmail.com>
2022-12-24 11:13:48 -0300

    Added: add task function Fix: new tasks non responsive Removed: Remaining files from class

--- main.js
+++ main.js
@@ -11,51 +11,50 @@ $(document).ready(function () {
         e.preventDefault();
 
         const nomeTarefa = $('#nome-tarefa');
-        const descricaoTarefa = $('descricao-tarefa');
+        const descricaoTarefa = $('#descricao-tarefa');
         $(`
         <tr>
-            <td class="descricao-controle" rowspan="1" data-toggle='mostrar-descricao'>
-                <i class="fa-solid fa-plus"></i>
+            <td class="descricao-controle" rowspan="2">
+                <i class="fa-solid fa-minus"></i>
             </td>
-            <td class="nome-tarefa" data-toggle="clica-titulo" value="1">test 2 task</td>
-            <td class="delete-tarefa" rowspan="1" data-toggle="deletar-tarefa">
+            <td class="nome-tarefa">${nomeTarefa.val()}</td>
+            <td class="delete-tarefa" rowspan="2">
                 <i class="fa-solid fa-trash-can"></i>
             </td>
         </tr>
         <tr>
-            <td class="descricao" data-toggle="clica-tarefa">test 2 task description</td>
+            <td class="descricao">${descricaoTarefa.val()}</td>
         </tr>
         `).appendTo('tbody');
     })
+});
 
-    //---------Detectar click para expandir ou retrair Descrição---------
-    $('[data-toggle="mostrar-descricao"]').click(function () {
-        if($(this).attr('rowspan') == 1){
-            expandirDescricao($(this));
-        } else {
-            retrairDescricao($(this));
-        }
-        
-    })
-    //--------Riscar valores da tarefa-------------------------------------
-    $('[data-toggle="clica-titulo"]').click(function () {
-        const descricaoTd = $(this).parent().next().children('.descricao');
-        $(this).addClass('terminada');
-        descricaoTd.addClass('terminada');
-    })
-    $('[data-toggle="clica-tarefa"]').click(function () {
-        const tituloTd = $(this).parent().prev().children('.nome-tarefa');
-        $(this).addClass('terminada');
-        tituloTd.addClass('terminada');
-    })
-    //--------Remover tarefa-------------------------------------------------
-    $('[data-toggle="deletar-tarefa"]').click(function() {
-        const linhaTabela = $(this).parent();
-        const proximaLinha = linhaTabela.next();
-        proximaLinha.remove();
-        linhaTabela.remove();
-    });
-
+//---------Detectar click para expandir ou retrair Descrição---------
+$(document).on('click', '.descricao-controle', function () {
+    if($(this).attr('rowspan') == 1){
+        expandirDescricao($(this));
+    } else {
+        retrairDescricao($(this));
+    }
+    
+})
+//--------Riscar valores da tarefa-------------------------------------
+$(document).on('click', '.nome-tarefa', function () {
+    const descricaoTd = $(this).parent().next().children('.descricao');
+    $(this).addClass('terminada');
+    descricaoTd.addClass('terminada');
+})
+$(document).on('click', '.descricao', function () {
+    const tituloTd = $(this).parent().prev().children('.nome-tarefa');
+    $(this).addClass('terminada');
+    tituloTd.addClass('terminada');
+})
+//--------Remover tarefa-------------------------------------------------
+$(document).on('click', '.delete-tarefa', function () {
+    const linhaTabela = $(this).parent();
+    const proximaLinha = linhaTabela.next();
+    proximaLinha.remove();
+    linhaTabela.remove();
 });
 //------------Func Expandir Desc Tabela----------------------------------
 function expandirDescricao(item) {
